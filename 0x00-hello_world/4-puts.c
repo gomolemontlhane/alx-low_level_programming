c413419771683abdff8b6c304277f9fe2d0bcb36
gomolemontlhane <gomolemontlhane@gmail.com>
2023-09-07 18:36:12 -0700

    updated code to fit betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
-int main() {
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints a message using the puts function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *message = "Programming is like building a multilingual puzzle";
+
+	puts(message);
+
+	return (0);
 }
