6b74b9b5de9627ae1a90955e4def111ffe68cb2e
gomolemontlhane <gomolemontlhane@gmail.com>
2023-09-13 15:40:22 -0700

    added print_last_digit function files

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -9,10 +9,17 @@ int main(void)
 {
 	int r;
 
-	print_last_digit(98);
-	print_last_digit(0);
+	r = print_last_digit(98);
+	_putchar('0' + r);
+	_putchar('\n');
+
+	r = print_last_digit(0);
+	_putchar('0' + r);
+	_putchar('\n');
+
 	r = print_last_digit(-1024);
 	_putchar('0' + r);
 	_putchar('\n');
+
 	return (0);
 }
