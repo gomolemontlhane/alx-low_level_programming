30482b49e233f68b24a342b093e4c07da9346534
gomolemontlhane <gomolemontlhane@gmail.com>
2023-09-12 20:41:44 -0700

    initial commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Function prototype for _putchar */
 int _putchar(char c);
+
+/* Function prototype for print_alphabet */
+void print_alphabet(void);
 
 #endif /* MAIN_H */
