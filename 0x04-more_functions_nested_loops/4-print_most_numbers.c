e2f4a74d1990a75e0767ba5906a41bc7f3783629
gomolemontlhane <gomolemontlhane@gmail.com>
2023-09-15 15:12:35 -0700

    initial commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints numbers 0 to 9 except 2 and 4, followed by a newline.
+ * print_most_numbers - prints numbers 0 to 9 except 2 and 4.
  */
 void print_most_numbers(void)
 {
