48a9e218a8e0aadbe89e6faa041e3e0b20bfb8cb
gomolemontlhane <gomolemontlhane@gmail.com>
2023-09-15 15:03:26 -0700

    initial commit

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes a character to the standard output (stdout)
+ * @c: The character to be written
+ *
+ * Return: On success, 1 is returned. On error, -1 is returned.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
