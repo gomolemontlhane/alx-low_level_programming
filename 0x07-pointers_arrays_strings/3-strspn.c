e1e5ae568ab55a190f7b87116928e2e4a81df8e4
gomolemontlhane <gomolemontlhane@gmail.com>
2023-09-22 19:44:58 -0700

    fixing 3-strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -5,7 +5,9 @@
  * @s: string to be searched
  * @accept: characters to be accepted
  *
- * Return: number of bytes in the initial segment of s which consist only of bytes from accept
+ * Return: number of bytes in the initial segment of s
+ * which consist only of bytes from accept.
+ *
  */
 unsigned int _strspn(char *s, char *accept)
 {
