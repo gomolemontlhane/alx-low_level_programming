6603391f878288bdac4287fa7e6857ec66f32408
gomolemontlhane <gomolemontlhane@gmail.com>
2023-09-29 17:53:45 -0700

    Add program to multiply two numbers
    Created a program (3-mul.c) that multiplies two numbers. If the program does not receive two arguments, it prints 'Error' and returns 1. Also included the necessary header file (main.h) with function prototypes.

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the program command-line arguments
+ *
+ * Return: 0 (Success) or 1 (Error)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+	{
+		int num1 = atoi(argv[1]);
+		int num2 = atoi(argv[2]);
+
+		printf("%d\n", num1 * num2);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
