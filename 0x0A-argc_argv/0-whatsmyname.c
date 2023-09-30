a833ea64adba84eb343be9988ee7eb8007067953
gomolemontlhane <gomolemontlhane@gmail.com>
2023-09-29 17:28:09 -0700

    implenting whatsmyname program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the program command-line arguments
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+		return (0);
+	}
+	return (1);
+}
