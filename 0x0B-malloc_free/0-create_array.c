10c58e9711409d660c30fe7e083d885523980e1b
gomolemontlhane <gomolemontlhane@gmail.com>
2023-10-03 14:51:48 -0700

    Implement create_array function with proper indentation and adherence to coding rules

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 
 /**
- * create_array - creates an array of chars and initializes it with a specific char
+ * create_array - creates an array of chars and
+ * initializes it with a specific char.
+ *
  * @size: the size of the array
  * @c: the char to initialize the array with
  *
