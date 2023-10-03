5de8b736fb7bb6b08a4f15288f6b38a48541ba1b
gomolemontlhane <gomolemontlhane@gmail.com>
2023-10-03 15:33:21 -0700

    Task 2: Add main and str_concat implementation

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: the first string
+ * @s2: the second string
+ *
+ * Return: pointer to a newly allocated space in memory containing
+ * the contents of s1 followed by the contents of s2 and null terminated,
+ * or NULL on failure
+ */
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int len1, len2, i, j;
+	char *result;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	len1 = 0;
+	while (s1[len1] != '\0')
+		len1++;
+
+	len2 = 0;
+	while (s2[len2] != '\0')
+		len2++;
+
+	result = malloc((len1 + len2 + 1) * sizeof(char));
+
+	if (result == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		result[i] = s1[i];
+
+	for (j = 0; j < len2; i++, j++)
+		result[i] = s2[j];
+
+	result[i] = '\0';
+
+	return (result);
+}
