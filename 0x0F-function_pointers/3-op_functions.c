8e2c67395e7a3bd3c7c7716915c07370fd9a6da4
gomolemontlhane <gomolemontlhane@gmail.com>
2023-10-11 16:38:36 -0700

    correcting 3-op_functions.c return statement

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -21,7 +21,7 @@ int op_add(int a, int b)
  */
 int op_sub(int a, int b)
 {
-	return a - b;
+	return (a - b);
 }
 
 /**
@@ -33,7 +33,7 @@ int op_sub(int a, int b)
  */
 int op_mul(int a, int b)
 {
-	return a * b;
+	return (a * b);
 }
 
 /**
@@ -46,7 +46,7 @@ int op_mul(int a, int b)
 int op_div(int a, int b)
 {
 	if (b != 0)
-		return a / b;
+		return (a / b);
 	else
 	{
 		printf("Error\n");
@@ -64,7 +64,7 @@ int op_div(int a, int b)
 int op_mod(int a, int b)
 {
 	if (b != 0)
-		return a % b;
+		return (a % b);
 	else
 	{
 		printf("Error\n");
