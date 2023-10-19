0bf8222ac26c6c34dd5d71177f1165c68d37030b
gomolemontlhane <gomolemontlhane@gmail.com>
2023-10-19 15:51:32 -0700

    Implement function to print elements of a linked list (Task 0) - 0-main.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ *print_list - Prints all the elements of a list_t list.
+ *@h: A pointer to the head of the list.
+ *Return: The number of nodes.
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		printf("[%u] %s\n", h->len, h->str ? h->str : "(nil)");
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
