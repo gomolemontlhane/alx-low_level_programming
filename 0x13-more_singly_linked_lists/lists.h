bdcfaf1db7502316255ff9fd100de838f976e405
gomolemontlhane <gomolemontlhane@gmail.com>
2023-10-24 14:23:21 -0700

    Implement task 1: List length

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <stdlib.h>
+#include <stddef.h>
 
 /**
  *struct listint_s - singly linked list
@@ -13,9 +13,13 @@ typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
-} listint_t;
+}
 
+listint_t;
+
+/*Function prototypes */
 size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 
 
 #endif /*LISTS_H */
