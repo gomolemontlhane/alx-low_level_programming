aab510078c00ad09a226a2b8c2dca873bdd5fd64
gomolemontlhane <gomolemontlhane@gmail.com>
2023-12-07 14:33:57 -0800

    adding task 2 and 3 solutions

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -17,9 +17,11 @@ typedef struct dlistint_s
 	struct dlistint_s *next;
 } dlistint_t;
 
-/*Function prototypes for Task 0 and Task 1 */
+/*Function prototypes for Task 0, Task 1, Task 2, and Task 3 */
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 
 
 #endif /*LISTS_H */
