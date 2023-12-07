b91bb1a9db07f8912647ebc33238385986e17702
gomolemontlhane <gomolemontlhane@gmail.com>
2023-12-07 14:18:14 -0800

    Add solution for Task 1

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -17,8 +17,9 @@ typedef struct dlistint_s
 	struct dlistint_s *next;
 } dlistint_t;
 
-/*Function prototype for Task 0 */
+/*Function prototypes for Task 0 and Task 1 */
 size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
 
 
 #endif /*LISTS_H */
