51a7f4ca20f03c61b86911955446f6c0f010048b
gomolemontlhane <gomolemontlhane@gmail.com>
2023-12-24 11:43:18 -0800

    fixing betty errros

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,7 +7,7 @@
  *
  *Return: A pointer to the newly created hash table, or NULL if it fails
  */
-hash_table_t* hash_table_create(unsigned long int size)
+hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
 	unsigned long int i;
@@ -17,7 +17,7 @@ hash_table_t* hash_table_create(unsigned long int size)
 		return (NULL);
 
 	new_table->size = size;
-	new_table->array = malloc(sizeof(hash_node_t*) * size);
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_table->array == NULL)
 	{
 		free(new_table);
